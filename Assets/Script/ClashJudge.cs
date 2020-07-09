1b4beaf1e825f59b2933b9f0343f8ffa604b9af2
takarahayato <e185734@ie.u-ryukyu.ac.jp>
2020-07-10 00:11:47 +0900

    add secens move

---
+++ Assets/Script/ClashJudge.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClashJudge : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        // Debug.Log("Clash3");
+     
+        if(collision.gameObject.CompareTag("Cube")){
+     Debug.Log("Clash");
+            SceneManager.LoadScene("GameOver");
+          }
+    }
+
+// Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+// Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
