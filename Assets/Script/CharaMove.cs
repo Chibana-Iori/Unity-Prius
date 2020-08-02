60b34767bb25b1a4ecf3379068c1189fd1efbab7
Iori Chibana <e185746@ie.u-ryukyu.ac.jp>
2020-08-02 23:41:58 +0900

    add score and MovePhone

--- Assets/Script/CharaMove.cs
+++ Assets/Script/CharaMove.cs
@@ -26,6 +26,10 @@ public class CharaMove : MonoBehaviour
     public float speedX;
     public float speedJump;
     public float accelerationZ;
+    bool Move;
+    private Vector3 touchStartPos;
+    private Vector3 touchEndPos;
+    string Direction;
     
     void Start()
     {
@@ -40,10 +44,22 @@ public class CharaMove : MonoBehaviour
     {
         if (controller.isGrounded) gravity = 10;
         //それぞれの矢印が押されたらそれぞれの関数を実行します。
-        if (Input.GetKeyDown("left")) MoveToLeft();
-        if (Input.GetKeyDown("right")) MoveToRight();
-        if (Input.GetKeyDown("space")) Jump();
-        if (Input.GetKeyDown("down")) Sliding();
+        //if (Input.GetKeyDown("left")) MoveToLeft();
+        //if (Input.GetKeyDown("right")) MoveToRight();
+        //if (Input.GetKeyDown("space")) Jump();
+        //if (Input.GetKeyDown("down")) Sliding();
+        if (Input.GetKeyDown(KeyCode.Mouse0)){
+            touchStartPos = new Vector3(Input.mousePosition.x,
+            Input.mousePosition.y,
+            Input.mousePosition.z);
+            }
+        if (Input.GetKeyUp(KeyCode.Mouse0)){
+        touchEndPos = new Vector3(Input.mousePosition.x,
+        Input.mousePosition.y,
+        Input.mousePosition.z);
+        GetDirection();
+        }
+
 
     
 
@@ -63,6 +79,35 @@ public class CharaMove : MonoBehaviour
         animator.SetBool("run", moveDirection.z > 0.0f);
         
     }
+    void FixedUpdate(){
+        if (Move){
+            switch (Direction){
+            //右移動
+            case "right":
+            MoveToRight();
+            Move=false;
+            break;
+            //左移動
+            case "left":
+            MoveToLeft();
+            Move=false;
+            break;
+            //ジャンプ
+            case "up":
+            Jump();
+            Move=false;
+            break;
+            //スライディング
+            case "down":
+            Sliding();
+            Move=false;
+            break;
+            case "touch":
+            break;
+            Move=false;
+        }}
+    }
+
 
     //新しく作った関数のそれぞれの処理。
     public void MoveToLeft()
@@ -100,7 +145,34 @@ public class CharaMove : MonoBehaviour
         }else{
             gravity = 50;
         }
-    
     }
+    void GetDirection(){
+        float directionX = touchEndPos.x - touchStartPos.x;
+        float directionY = touchEndPos.y - touchStartPos.y;
+        if (Mathf.Abs(directionY) < Mathf.Abs(directionX)){
+            if (30 < directionX){
+                //右向きにフリック
+                Direction = "right";
+                }else if (-30 > directionX){
+                    //左向きにフリック
+                    Direction = "left";
+                    }
+                    }
+                    else if (Mathf.Abs(directionX)<Mathf.Abs(directionY)){
+                        if (30 < directionY){
+                            //上向きにフリック
+                            Direction = "up";
+                            }else if (-30 > directionY){
+                                //下向きのフリック
+                                Direction = "down";
+                                }
+                                }else{
+                                    //タッチを検出
+                                    Direction = "touch";
+                                    }
+                                    Move=true;
+        }
+
+
 
 }
